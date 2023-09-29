d70cbbb69e704847f1a5337ff76ecf5086296acc
Park Chan <zxc88kr@naver.com>
2023-09-30 06:15:59 +0900

    feat: Practice 02 for Other Class

--- ex02/app/src/main/java/com/example/ex02/MainActivity.java
+++ ex02/app/src/main/java/com/example/ex02/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -66,5 +70,86 @@ public class MainActivity extends AppCompatActivity {
         int myArray[] = new int[]{1, 2, 3, 4, 5};
         System.out.println("배열의 3번째 값: " + myArray[2]);
         */
+
+        /*
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+        */
+
+        /*
+        String s = " Java Programming ";
+        System.out.println("s.length(): " + s.length());
+        System.out.println("s.charAt(1): " + s.charAt(1));
+        System.out.println("s.substring(6): " + s.substring(6));
+        System.out.println("s.substring(5, 13): " + s.substring(5, 13));
+        System.out.println("s.indexOf('P'): " + s.indexOf('P'));
+        System.out.println("s.toLowerCase(): " + s.toLowerCase());
+        System.out.println("s.toUpperCase(): " + s.toUpperCase());
+        */
+
+        /*
+        int result = mul(3, 5);
+        System.out.println("사용자 정의 함수 mul()");
+        System.out.println("두 수의 곱은 " + result);
+        */
+
+        /*
+        Member myObj = new Member();
+        myObj.name = "홍길동";
+        myObj.age = 20;
+        myObj.gender = 'F';
+        myObj.printMemberInfo();
+
+        Employee myEmp = new Employee();
+        */
+
+        List<String> lists = new ArrayList();
+        lists.add("월요일");
+        lists.add("화요일");
+        lists.add("수요일");
+        lists.add("목요일");
+        lists.add("금요일");
+        System.out.println(lists.size());
+        System.out.println(lists.get(0));
+        System.out.println(lists.contains("토요일"));
+        System.out.println(lists.indexOf("화요일"));
+    }
+
+    /*
+    int mul(int a, int b) {
+        int number = a * b;
+        return number;
+    }
+    */
+}
+
+/*
+class Member {
+    String name = "";
+    int age = 0;
+    char gender = 'M';
+
+    void printMemberInfo() {
+        System.out.println("이름: " + name);
+        System.out.println("나이: " + age);
+        System.out.println("성별: " + gender);
     }
 }
+
+class Employee {
+    int id = 202201;
+    String name = "홍길순";
+
+    Employee() {
+        System.out.println("Employee 아이디: " + id);
+        System.out.println("Employee 이름: " + name);
+    }
+}
+*/
