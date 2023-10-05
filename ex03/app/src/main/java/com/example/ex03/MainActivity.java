816436662600e41f8baae528cc12937a685c9766
Park Chan <zxc88kr@naver.com>
2023-10-06 03:08:49 +0900

    feat: New Practive 3

--- ex03/app/src/main/java/com/example/ex03/MainActivity.java
+++ ex03/app/src/main/java/com/example/ex03/MainActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
     /*
     TextView tvObj1;
     TextView tvObj2;
+    */
+
+    /*
+    Button btn1;
+    TextView tvObj1;
+    TextView tvObj2;
+    String num1, num2;
+    Integer result;
     */
 
     @Override
@@ -50,6 +59,53 @@ public class MainActivity extends AppCompatActivity {
         tvObj1 = findViewById(R.id.num1);
         tvObj2 = findViewById(R.id.num2);
         */
+
+        /*
+        tvObj1 = findViewById(R.id.num1);
+        tvObj2 = findViewById(R.id.num2);
+        btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num1 = tvObj1.getText().toString();
+                num2 = tvObj2.getText().toString();
+                result = Integer.parseInt(num1) + Integer.parseInt(num2);
+                Toast.makeText(getApplicationContext(), String.valueOf(result), Toast.LENGTH_LONG).show();
+            }
+        });
+        */
+
+        EditText nameObj, passwordObj, emailObj, dateObj, phoneObj;
+        Button btnObj;
+        TextView resultObj;
+
+        nameObj = findViewById(R.id.edtName);
+        passwordObj = findViewById(R.id.edtPasswd);
+        emailObj = findViewById(R.id.edtEmail);
+        dateObj = findViewById(R.id.edtDate);
+        phoneObj = findViewById(R.id.edtPhone);
+        btnObj = findViewById(R.id.btnSend);
+        resultObj = findViewById(R.id.txtResult);
+
+        btnObj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (nameObj.getText().toString().isEmpty() ||
+                    passwordObj.getText().toString().isEmpty() ||
+                    emailObj.getText().toString().isEmpty() ||
+                    dateObj.getText().toString().isEmpty() ||
+                    phoneObj.getText().toString().isEmpty()
+                ) {
+                    resultObj.setText("항목을 입력해주세요");
+                } else {
+                    resultObj.setText("성명: " + nameObj.getText().toString() + "\n" +
+                            "비밀번호: " + passwordObj.getText().toString() + "\n" +
+                            "이메일: " + emailObj.getText().toString() + "\n" +
+                            "생년월일: " + dateObj.getText().toString() + "\n" +
+                            "연락처: " + phoneObj.getText().toString());
+                }
+            }
+        });
     }
 
     /*
