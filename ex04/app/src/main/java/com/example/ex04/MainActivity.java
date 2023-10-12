488583f206dc2d55f95c68825f471a2cfaa28c48
Park Chan <zxc88kr@naver.com>
2023-10-12 16:57:08 +0900

    feat: Set Layout for Custom

--- ex04/app/src/main/java/com/example/ex04/MainActivity.java
+++ ex04/app/src/main/java/com/example/ex04/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.ex04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        LinearLayout myLayout = new LinearLayout(this);
+        myLayout.setOrientation(LinearLayout.HORIZONTAL);
+        myLayout.setBackgroundColor(Color.rgb(0, 255, 0));
+
+        Button b1 = new Button(this);
+        b1.setText("버튼1");
+        myLayout.addView(b1);
+
+        setContentView(myLayout, params);
     }
 }
