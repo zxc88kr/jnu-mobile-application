d85e816143499af9baded959751b32ceec6eb532
Park Chan <zxc88kr@naver.com>
2023-10-26 06:04:38 +0900

    feat: EditText Practice

--- midterm/app/src/main/java/com/example/midterm/MainActivity.java
+++ midterm/app/src/main/java/com/example/midterm/MainActivity.java
@@ -7,17 +7,20 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = (TextView) findViewById(R.id.textView);
+        textView = (TextView) findViewById(R.id.textView);
         Integer a = Integer.parseInt(textView.getText().toString());
         System.out.println(a);
         textView.setText("1345151dadaf");
@@ -26,9 +29,15 @@ public class MainActivity extends AppCompatActivity {
         textView.setTextSize(20);
         textView.setTypeface(Typeface.SERIF);
         textView.setTypeface(Typeface.MONOSPACE);
+
+        editText = (EditText) findViewById(R.id.editText);
     }
 
-    public void onButtonClick(View view) {
+    public void onButtonClick1(View view) {
         Toast.makeText(getApplicationContext(), String.valueOf(3), Toast.LENGTH_LONG).show();
+    }
+
+    public void onButtonClick2(View view) {
+        Toast.makeText(getApplicationContext(), editText.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 }
