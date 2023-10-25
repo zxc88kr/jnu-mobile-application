94ab02be17617484ac640bd90ff6268b2e56ff33
Park Chan <zxc88kr@naver.com>
2023-10-26 05:17:47 +0900

    feat: TextView Practice

--- midterm/app/src/main/java/com/example/midterm/MainActivity.java
+++ midterm/app/src/main/java/com/example/midterm/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        System.out.println(textView.getText());
+        textView.setText("1345151dadaf");
+        textView.setTextColor(Color.parseColor("#ff0000"));
+        textView.setGravity(Gravity.LEFT);
+        textView.setTextSize(20);
+        textView.setTypeface(Typeface.SERIF);
+        textView.setTypeface(Typeface.MONOSPACE);
     }
 }
