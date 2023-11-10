fdc3fc5c62c0b23720881f64a991d261c70e4bcc
Park Chan <zxc88kr@naver.com>
2023-11-10 19:26:58 +0900

    feat: Page 1 Complete

--- project02/app/src/main/java/com/example/project02/MainActivity.java
+++ project02/app/src/main/java/com/example/project02/MainActivity.java
@@ -8,27 +8,43 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton btn1, btn2, btn3, btn4;
+    ImageButton coverBtn1, coverBtn2;
+    ImageButton menuBtn1, menuBtn2, menuBtn3, menuBtn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = findViewById(R.id.imageBtn1);
-        btn2 = findViewById(R.id.imageBtn2);
-        btn3 = findViewById(R.id.imageBtn3);
-        btn4 = findViewById(R.id.imageBtn4);
+        coverBtn1 = findViewById(R.id.coverBtn1);
+        coverBtn2 = findViewById(R.id.coverBtn2);
+
+        menuBtn1 = findViewById(R.id.menuBtn1);
+        menuBtn2 = findViewById(R.id.menuBtn2);
+        menuBtn3 = findViewById(R.id.menuBtn3);
+        menuBtn4 = findViewById(R.id.menuBtn4);
+
+        getSupportActionBar().hide();
+    }
+
+    public void onClickCover(View view) {
+        if (coverBtn1.getVisibility() == View.VISIBLE) {
+            coverBtn2.setVisibility(View.VISIBLE);
+            coverBtn1.setVisibility(View.INVISIBLE);
+        } else if (coverBtn2.getVisibility() == View.VISIBLE) {
+            coverBtn1.setVisibility(View.VISIBLE);
+            coverBtn2.setVisibility(View.INVISIBLE);
+        }
     }
 
     public void onClickMenu(View view) {
-        if (view.getId() == btn1.getId()) {
+        if (view.getId() == menuBtn1.getId()) {
             Toast.makeText(getApplicationContext(), "도서목록 버튼이 클릭되었습니다", Toast.LENGTH_SHORT).show();
-        } else if (view.getId() == btn2.getId()) {
+        } else if (view.getId() == menuBtn2.getId()) {
             Toast.makeText(getApplicationContext(), "동영상강좌 버튼이 클릭되었습니다", Toast.LENGTH_SHORT).show();
-        } else if (view.getId() == btn3.getId()) {
+        } else if (view.getId() == menuBtn3.getId()) {
             Toast.makeText(getApplicationContext(), "고객센터 버튼이 클릭되었습니다", Toast.LENGTH_SHORT).show();
-        } else if (view.getId() == btn4.getId()) {
+        } else if (view.getId() == menuBtn4.getId()) {
             Toast.makeText(getApplicationContext(), "마이페이지 버튼이 클릭되었습니다", Toast.LENGTH_SHORT).show();
         }
     }
