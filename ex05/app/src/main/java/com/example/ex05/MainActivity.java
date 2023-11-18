4ec594d6fedcfc4eb85cc6df7598e5b4a7f41eec
Park Chan <zxc88kr@naver.com>
2023-11-19 04:29:41 +0900

    feat: Checkbox

--- ex05/app/src/main/java/com/example/ex05/MainActivity.java
+++ ex05/app/src/main/java/com/example/ex05/MainActivity.java
@@ -2,51 +2,68 @@ package com.example.ex05;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    ImageButton btObj1, btObj2, btObj3;
-    ImageView ivObj;
+    CheckBox cbObj1, cbObj2, cbObj3;
+    Button btnObj;
+    String result = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btObj1 = findViewById(R.id.imageButton1);
-        btObj2 = findViewById(R.id.imageButton2);
-        btObj3 = findViewById(R.id.imageButton3);
-        ivObj = findViewById(R.id.imageView2);
+        cbObj1=findViewById(R.id.checkBox1);
+        cbObj2=findViewById(R.id.checkBox2);
+        cbObj3=findViewById(R.id.checkBox3);
+        btnObj=findViewById(R.id.button);
 
-        btObj1.setOnClickListener(new View.OnClickListener() {
+        btnObj.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ivObj.setVisibility(View.VISIBLE);
-                ivObj.setImageResource(R.drawable.answer1);
-                Toast.makeText(getApplicationContext(), "틀렸습니다", Toast.LENGTH_SHORT).show();
+                result = "selected cb: \n";
+                if (cbObj1.isChecked())
+                    result += "checkbox1,";
+                if (cbObj2.isChecked())
+                    result += "checkbox2,";
+                if (cbObj3.isChecked())
+                    result += "checkbox3,";
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
             }
         });
 
-        btObj2.setOnClickListener(new View.OnClickListener() {
+        cbObj1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                ivObj.setVisibility(View.VISIBLE);
-                ivObj.setImageResource(R.drawable.answer2);
-                Toast.makeText(getApplicationContext(), "맞았습니다", Toast.LENGTH_SHORT).show();
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    Toast.makeText(getApplicationContext(), "cb1 selected", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(getApplicationContext(), "cb1 unselected", Toast.LENGTH_LONG).show();
             }
         });
 
-        btObj3.setOnClickListener(new View.OnClickListener() {
+        cbObj2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                ivObj.setVisibility(View.VISIBLE);
-                ivObj.setImageResource(R.drawable.answer3);
-                Toast.makeText(getApplicationContext(), "틀렸습니다", Toast.LENGTH_SHORT).show();
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    Toast.makeText(getApplicationContext(), "cb2 selected", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(getApplicationContext(), "cb2 unselected", Toast.LENGTH_LONG).show();
+            }
+        });
+        cbObj3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    Toast.makeText(getApplicationContext(), "cb3 selected", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(getApplicationContext(), "cb3 unselected", Toast.LENGTH_LONG).show();
             }
         });
     }
