5b6a788e9dba8b23d151bc925904300c5ba646a4
Park Chan <zxc88kr@naver.com>
2023-11-19 08:21:36 +0900

    feat: Splash Image

--- ex06/app/src/main/java/com/example/ex06/MainActivity.java
+++ ex06/app/src/main/java/com/example/ex06/MainActivity.java
@@ -6,19 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnObj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        btnObj = findViewById(R.id.button);
+        btnObj.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                EditText editText01 = findViewById(R.id.editText01);
+                EditText editText02 = findViewById(R.id.editText02);
+                intent.putExtra("userId", editText01.getText().toString());
+                intent.putExtra("userPw", editText02.getText().toString());
                 startActivity(intent);
             }
         });
--- ex06/app/src/main/java/com/example/ex06/SubActivity.java
+++ ex06/app/src/main/java/com/example/ex06/SubActivity.java
@@ -2,13 +2,23 @@ package com.example.ex06;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
+    Button btnObj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        TextView idObj= findViewById(R.id.textView01);
+        TextView pwObj  = findViewById(R.id.textView02);
+        Intent intent = getIntent();
+        idObj.setText(intent.getStringExtra("userId"));
+        pwObj.setText(intent.getStringExtra("userPw"));
     }
 }
