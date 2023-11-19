33c946abe2fc7fefad13f4f9b7343412c6e6ff6b
Park Chan <zxc88kr@naver.com>
2023-11-19 12:45:02 +0900

    feat: Update

--- project02/app/src/main/java/com/example/project02/BookActivity.java
+++ project02/app/src/main/java/com/example/project02/BookActivity.java
@@ -9,5 +9,7 @@ public class BookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
+
+        getSupportActionBar().hide();
     }
 }
