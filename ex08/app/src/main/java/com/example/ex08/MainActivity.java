e3e32b56275932f31f9b162fa15065e967772a37
Park Chan <zxc88kr@naver.com>
2023-12-02 06:23:27 +0900

    feat: Alert Box

--- ex08/app/src/main/java/com/example/ex08/MainActivity.java
+++ ex08/app/src/main/java/com/example/ex08/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.ex08;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+                alertDialog.setTitle("AlertDialog");
+                alertDialog.setMessage("앱을 종료하시겠습니까?");
+                alertDialog.setPositiveButton("예", null);
+                alertDialog.setNegativeButton("아니오", null);
+                alertDialog.show();
+            }
+        });
     }
 }
