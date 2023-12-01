7bd3fff179f6eb9ff6fbe5fcc8b86ad7f30d7966
Park Chan <zxc88kr@naver.com>
2023-12-02 07:16:48 +0900

    feat: Dialog

--- ex08/app/src/main/java/com/example/ex08/MainActivity.java
+++ ex08/app/src/main/java/com/example/ex08/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.ex08;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,8 +23,19 @@ public class MainActivity extends AppCompatActivity {
                 AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
                 alertDialog.setTitle("AlertDialog");
                 alertDialog.setMessage("앱을 종료하시겠습니까?");
-                alertDialog.setPositiveButton("예", null);
-                alertDialog.setNegativeButton("아니오", null);
+                alertDialog.setPositiveButton("예", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                alertDialog.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "취소합니다", Toast.LENGTH_SHORT).show();
+                        dialog.cancel();
+                    }
+                });
                 alertDialog.show();
             }
         });
