00ca647d02b0bff220e394a61eff704995b93520
Park Chan <zxc88kr@naver.com>
2023-12-01 17:49:43 +0900

    feat: Menu Items using Java Code

--- ex07/app/src/main/java/com/example/ex07/MainActivity.java
+++ ex07/app/src/main/java/com/example/ex07/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.ex07;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.SubMenu;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        SubMenu sub = menu.addSubMenu("편집하기");
+        sub.add(Menu.NONE, Menu.FIRST + 0, Menu.NONE, "복사하기");
+        sub.add(Menu.NONE, Menu.FIRST + 10, Menu.NONE, "잘라내기");
+        sub.add(Menu.NONE, Menu.FIRST + 20, Menu.NONE, "붙여넣기");
+
+        menu.add(Menu.NONE, Menu.FIRST + 1, Menu.NONE, "검색하기");
+        menu.add(Menu.NONE, Menu.FIRST + 2, Menu.NONE, "저장하기");
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
