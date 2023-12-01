0d51f1d846ace064fef139719789f52499aebd92
Park Chan <zxc88kr@naver.com>
2023-12-02 05:41:27 +0900

    feat: Twitter

--- ex07/app/src/main/java/com/example/ex07/MainActivity.java
+++ ex07/app/src/main/java/com/example/ex07/MainActivity.java
@@ -1,30 +1,18 @@
 package com.example.ex07;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-    LinearLayout layout;
-    TextView text;
-    androidx.appcompat.widget.Toolbar toolbar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        layout = (LinearLayout) findViewById(R.id.background);
-        text = (TextView) findViewById(R.id.text);
-        toolbar = findViewById(R.id.toolbar);
-
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 
@@ -32,27 +20,5 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu01:
-                text.setTextColor(Color.parseColor("#ffff00"));
-                break;
-            case R.id.menu02:
-                text.setTextColor(Color.parseColor("#ffffff"));
-                break;
-            case R.id.menu03:
-                layout.setBackgroundColor(Color.parseColor("#ff0000"));
-                break;
-            case R.id.menu04:
-                layout.setBackgroundColor(Color.parseColor("#00ff00"));
-                break;
-            case R.id.menu05:
-                layout.setBackgroundColor(Color.parseColor("#0000ff"));
-                break;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
