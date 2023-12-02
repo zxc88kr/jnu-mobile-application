891e7906bd14b403548e969735c042d94af6d15c
Park Chan <zxc88kr@naver.com>
2023-12-02 15:52:06 +0900

    feat: TimePickerDialog

--- ex08/app/src/main/java/com/example/ex08/MainActivity.java
+++ ex08/app/src/main/java/com/example/ex08/MainActivity.java
@@ -2,11 +2,11 @@ package com.example.ex08;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
@@ -23,18 +23,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Calendar calendar = Calendar.getInstance();
-                int day = calendar.get(Calendar.DAY_OF_MONTH);
-                int month = calendar.get(Calendar.MONTH);
-                int year = calendar.get(Calendar.YEAR);
+                int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                int minute = calendar.get(Calendar.MINUTE);
 
-                DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+                TimePickerDialog.OnTimeSetListener listener = new TimePickerDialog.OnTimeSetListener() {
                     @Override
-                    public void onDateSet(DatePicker view, int year, int month, int day) {
-                        Toast.makeText(MainActivity.this, "선택날짜: " + year + "년 " + (month + 1) + "월 " + day + "일", Toast.LENGTH_SHORT).show();
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Toast.makeText(MainActivity.this, "선택시간: " + hourOfDay + "시 " + minute + "분", Toast.LENGTH_SHORT).show();
                     }
                 };
-                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, listener, year, month, day);
-                datePickerDialog.show();
+                TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, listener, hour, minute, true);
+                timePickerDialog.show();
             }
         });
     }
