386174ce62431d12cb0821ee3b033891176065dc
Park Chan <zxc88kr@naver.com>
2023-12-03 05:53:32 +0900

    feat: Date and Time Example

--- ex08/app/src/main/java/com/example/ex08/MainActivity.java
+++ ex08/app/src/main/java/com/example/ex08/MainActivity.java
@@ -2,15 +2,15 @@ package com.example.ex08;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TimePicker;
-import android.widget.Toast;
 
 import java.util.Calendar;
-
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -18,8 +18,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        EditText etDate = findViewById(R.id.editTextDate);
+        EditText etTime = findViewById(R.id.editTextTime);
+
+        etDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        String selMonth;
+                        String selDay;
+
+                        if ((month + 1) < 10) {
+                            selMonth = "0" + (month + 1);
+                        } else {
+                            selMonth = Integer.toString(month + 1);
+                        }
+
+                        if (dayOfMonth < 10) {
+                            selDay = "0" + dayOfMonth;
+                        } else {
+                            selDay = Integer.toString(dayOfMonth);
+                        }
+
+                        etDate.setText(Integer.toString(year) + "/" + selMonth + "/" + selDay);
+                    }
+                };
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, listener, year, month, day);
+                datePickerDialog.setTitle("날짜를 선택하세요");
+                datePickerDialog.show();
+            }
+        });
+
+        etTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Calendar calendar = Calendar.getInstance();
@@ -29,10 +66,21 @@ public class MainActivity extends AppCompatActivity {
                 TimePickerDialog.OnTimeSetListener listener = new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        Toast.makeText(MainActivity.this, "선택시간: " + hourOfDay + "시 " + minute + "분", Toast.LENGTH_SHORT).show();
+                        String state = "AM";
+                        int selHour = hourOfDay;
+                        int selMinute = minute;
+
+                        if (selHour > 12) {
+                            state = "PM";
+                            selHour -= 12;
+                        }
+
+                        etTime.setText(state + " " + selHour + "시 " + selMinute + "분");
                     }
                 };
+
                 TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, listener, hour, minute, true);
+                timePickerDialog.setTitle("시간을 선택하세요");
                 timePickerDialog.show();
             }
         });
