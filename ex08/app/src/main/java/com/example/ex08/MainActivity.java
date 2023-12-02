1ce6885d0787f5c5538a83768f131c925dea5f74
Park Chan <zxc88kr@naver.com>
2023-12-02 15:34:24 +0900

    feat: DatePickerDialog

--- ex08/app/src/main/java/com/example/ex08/MainActivity.java
+++ ex08/app/src/main/java/com/example/ex08/MainActivity.java
@@ -1,70 +1,40 @@
 package com.example.ex08;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
 
 public class MainActivity extends AppCompatActivity {
-    int selectNum = 0;
-    int oldNum = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView imageView = findViewById(R.id.imageView);
-        TextView textView = findViewById(R.id.textView);
         Button button = findViewById(R.id.button);
-
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String listItems[] = {"아기상어", "엄마상어", "아빠상어"};
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
 
-                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
-                alertDialog.setTitle("상어가족");
-
-                alertDialog.setSingleChoiceItems(listItems, oldNum, new DialogInterface.OnClickListener() {
+                DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
                     @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        switch (which) {
-                            case 0: selectNum = 0; break;
-                            case 1: selectNum = 1; break;
-                            case 2: selectNum = 2; break;
-
-                        }
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        Toast.makeText(MainActivity.this, "선택날짜: " + year + "년 " + (month + 1) + "월 " + day + "일", Toast.LENGTH_SHORT).show();
                     }
-                });
-
-                alertDialog.setPositiveButton("예", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        switch (selectNum) {
-                            case 0: imageView.setImageResource(R.drawable.baby); break;
-                            case 1: imageView.setImageResource(R.drawable.mom); break;
-                            case 2: imageView.setImageResource(R.drawable.papa); break;
-                        }
-                        textView.setText(listItems[selectNum]);
-                        oldNum = selectNum;
-                    }
-                });
-
-                alertDialog.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-
-                alertDialog.show();
+                };
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, listener, year, month, day);
+                datePickerDialog.show();
             }
         });
     }
